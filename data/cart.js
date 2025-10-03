fc03c0b7a40461ee98de3a663066b6086c790848
sadzoyiedem <stephenadzoyi31@gmail.com>
2025-10-03 22:34:02 +0000

    localStorage syntax

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,4 @@
-export let cart = JSON.parse(localStorage.getItem('cart') || 
-[
-  {
+export let cart = JSON.parse(localStorage.getItem('cart') || [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
   }, {
