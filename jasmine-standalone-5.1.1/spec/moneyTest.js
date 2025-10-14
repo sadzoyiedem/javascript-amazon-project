e665e6c32dcb19d4d691d95f1992a5a3adf75d48
sadzoyiedem <stephenadzoyi31@gmail.com>
2025-10-14 16:21:05 +0000

    Removed deault tests and added money tests

---
+++ jasmine-standalone-5.1.1/spec/moneyTest.js
@@ -0,0 +1,21 @@
+import { formatCurrency } from "../../scripts/utils/money.js";
+
+describe('Test suite: formatCurrency', () =>{
+  it('converts cents to dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+
+  it('works with zero', () =>{
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+
+  it('rounds numbers then converts to dollars', () => {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+  
+  it('rounds number then converts to dollars', () =>{
+    expect(formatCurrency(200.3)).toEqual('2.00');
+
+  });
+  
+});
